68442e373ed5fed29665b606dfca99eb1fb728e2
cmac1223 <chrismac539@gmail.com>
2017-05-09 14:34:42 -0400

    html buttons

---
+++ JS/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+$('.b01').on('click', function(){
+  alert('Test');
+})
+$('.b03').on('click', function(){
+  alert('Test');
+})
+$('.b04').on('click', function(){
+  alert('Test');
+})
+$('.b05').on('click', function(){
+  alert('Test');
+})
+$('.b06').on('click', function(){
+  alert('Test');
+})
+
+
+});
