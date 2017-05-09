4978fd0a66d9374bb72f8765a0152c3e0e59324b
cmac1223 <chrismac539@gmail.com>
2017-05-09 17:44:38 -0400

    colorless button

--- JS/script.js
+++ JS/script.js
@@ -1,35 +1,40 @@
 $(document).ready(function(){
 
-// $('.b01').on('click', function(){
-//   alert('Test');
-// })
-// $('.b02').on('click', function(){
-//   alert('Test');
-// })
-// $('.b03').on('click', function(){
-//   alert('Test');
-// })
-// $('.b04').on('click', function(){
-//   alert('Test');
-// })
-// $('.b05').on('click', function(){
-//   alert('Test');
-// })
-// $('.b06').on('click', function(){
-//   alert('Test');
+//  $('#b01').on('click', function(){
+//    alert('Test');
+//  })
+//  $('#b02').on('click', function(){
+//    alert('Test');
+//  })
+//  $('#b03').on('click', function(){
+//    alert('Test');
+//  })
+//  $('#b04').on('click', function(){
+//    alert('Test');
+//  })
+//  $('#b05').on('click', function(){
+//    alert('Test');
+//  })
+//  $('#b06').on('click', function(){
+//    alert('Test');
+//  })
+
+// $('.btn').on('click', function(){
+//   alert('YES!');
 // })
 
-$('.btn').on('click', function(){
-  alert('YES!');
-})
-
-$('#container').on('click', function (event) {
+ $('#player1').on('click', function(event){
   event.stopPropagation();
   var theButtonColor = $(event.target).css('background-color');
-  $('.b01).css('background-color', theButtonColor);
+  $('.b01').css('background-color', theButtonColor);
+ })
+
+// $('.row1').click(function(){
+//    $('.b01').css(“color”, “red”);
 });
 
-  $()
-
-
 });
+
+
+
+
