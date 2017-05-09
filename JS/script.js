705b622db60fd17524d64d6241c5e28526af6ffe
cmac1223 <chrismac539@gmail.com>
2017-05-09 16:17:39 -0400

    update

--- JS/script.js
+++ JS/script.js
@@ -1,20 +1,35 @@
 $(document).ready(function(){
 
-$('.b01').on('click', function(){
-  alert('Test');
+// $('.b01').on('click', function(){
+//   alert('Test');
+// })
+// $('.b02').on('click', function(){
+//   alert('Test');
+// })
+// $('.b03').on('click', function(){
+//   alert('Test');
+// })
+// $('.b04').on('click', function(){
+//   alert('Test');
+// })
+// $('.b05').on('click', function(){
+//   alert('Test');
+// })
+// $('.b06').on('click', function(){
+//   alert('Test');
+// })
+
+$('.btn').on('click', function(){
+  alert('YES!');
 })
-$('.b03').on('click', function(){
-  alert('Test');
-})
-$('.b04').on('click', function(){
-  alert('Test');
-})
-$('.b05').on('click', function(){
-  alert('Test');
-})
-$('.b06').on('click', function(){
-  alert('Test');
-})
+
+$('#container').on('click', function (event) {
+  event.stopPropagation();
+  var theButtonColor = $(event.target).css('background-color');
+  $('.b01).css('background-color', theButtonColor);
+});
+
+  $()
 
 
 });
