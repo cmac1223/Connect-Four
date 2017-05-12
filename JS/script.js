5b7b83da02ceb22a6a7ff35f3b883abc20c666d5
cmac1223 <chrismac539@gmail.com>
2017-05-12 11:13:44 -0400

    update start at 0row complete

--- JS/script.js
+++ JS/script.js
@@ -1,22 +1,34 @@
 $(document).ready(function(){
      
-
+const gameBoard = [
+    [{},{},{},{},{},{},{}],
+    [{},{},{},{},{},{},{}],
+    [{},{},{},{},{},{},{}],
+    [{},{},{},{},{},{},{}],
+    [{},{},{},{},{},{},{}],
+    [{},{},{},{},{},{},{}],
+    [{},{},{},{},{},{},{}]
+];
     
     
-    var player1 = {
+    const Player1 = {
         class: 'player1',
         background: 'blue'
     };
 
 
 
-    var player2 = {
+    const Player2 = {
         class: 'player2',
         background: 'red'
     }
 
-
-    var currentPlayer = player1;
+    for (var i = 0; i < gameBoard.length; i++) {
+        for (var j = 0; j < 7; j++) {
+            gameBoard[i][j].player = null;
+        }
+    }
+    var currentPlayer = Player1;
 
     //target id want x & y values
     //create a click function when each button is clicked
@@ -36,21 +48,25 @@ $(document).ready(function(){
             console.log(y);
 
 
-            for(var i = 6; i >= 0; i--) {
+            for(var i = 0; i < 6; i++) {
                 console.log(gameBoard[x][i].player);
                 if(!gameBoard[x][i].player){
-                    
-                    gameBoard[x][i] = currentPlayer;
+                    console.log('x is ' + x);
+                    console.log('i is ' + i);
+                    console.log(gameBoard);
+                    gameBoard[x][i].player = currentPlayer;
                     //jquery to change class to currentplayer color class switch
                     // console.log(currentPlayer)
-                   $(event.target).addClass(currentPlayer.class);
+                    var $cell = $(`#${x}-${i}`);
+                    $cell.addClass(currentPlayer.class);
+                   $(`#${x}-${i}`).addClass(currentPlayer.class);
                    
                    console.log('Class: ' + currentPlayer.class);
                   
-                if(currentPlayer === player1){
-                    currentPlayer = player2;
-                } else if (currentPlayer === player2){
-                    currentPlayer = player1;
+                if(currentPlayer === Player1){
+                    currentPlayer = Player2;
+                } else if (currentPlayer === Player2){
+                    currentPlayer = Player1;
                 }
         
                      break;
@@ -62,114 +78,8 @@ $(document).ready(function(){
     
 });
 
-var gameBoard = [
-    [{},{},{},{},{},{},{}],
-    [{},{},{},{},{},{},{}],
-    [{},{},{},{},{},{},{}],
-    [{},{},{},{},{},{},{}],
-    [{},{},{},{},{},{},{}],
-    [{},{},{},{},{},{},{}],
-    [{},{},{},{},{},{},{}]
-];
 
 
-
-
-
-
-
-// function initialize() {
-//     $('.cell42 ').on('click', function(){
-//         console.log('callback fired');
-//         var $cells = $('.cell42');
-//         console.log($cells);
-//         for (var i = 0; i < $cells.length; i++) {
-//             console.log('loop hit');
-//             var cell42 = $cells[i];
-//             console.log(cell42);
-//             var x = parseInt(cell42.getAttribute(['id'])[0]);
-//             console.log(x);
-//             var y = parseInt(cell42.getAttribute(['id'])[2]);
-//             console.log(y);
-//             gameBoard[x][y].player = cell42.getAttribute(['currentPlayer'])
-        
-//         }
-// });
-
-
-    
-    
-
-
-
-
-
- 
-    
-
-
-//     var $cells = $('cell42')
-//     for (var i = 0; i < $cells.length; i++) {
-//         var cell42 = $cells[i];
-//         cell42.attr[i]
-//         var x = parseInt(cell42.attr['id'][0]);
-//         console.log(x);
-//         var y = parseInt(cell42.attr['id'][2]);
-//         console.log(y);
-//         gameBoard[x][y].player = cell42.attr['currentPlayer']
-    
-// }
-// });
-
-
-
-  
-  
-
-
-
-
-
-// var gameInfo = {
-//             firstPlayerName: "Player1",
-//             firstPlayerColor: "Black",
-//             secondPlayerName: "Player2",
-//             secondPlayerColor: "Red",
-//             firstPlayerToStart: "Black",
-//             currentPlayer: "Waiting on ",
-//             winner: 4,
-//         };
-
-// var gameBoard = [[x,x,x,x,x,x,x],
-//                  [x,x,x,x,x,x,x],
-//                  [x,x,x,x,x,x,x],
-//                  [x,x,x,x,x,x,x],
-//                  [x,x,x,x,x,x,x],
-//                  [x,x,x,x,x,x,x],
-//                 ];        
-
-//   $('#player1').css('background-color', theButtonColor);
-//  })
-
-//  $('#player2').on('click', function(event){
-//   event.stopPropagation();
-//   var theButtonColor = $(event.target).css('background-color');
-//   $('.button2').css('background-color', theButtonColor);
- 
-// });
- 
-//  var moveTracker = [];
-//  var rowX = [];
-//  var columnY = ["b01",];
-
-// $('x').on('click', function(){
-    
-//   ]
-//})
-
-//var x = ["button1"]
-
-//var y = ["b01"]
 
 
 
